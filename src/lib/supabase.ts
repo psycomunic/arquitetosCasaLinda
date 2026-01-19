add0ffbe27dc1cc1414fe6d22e52dd0268705db8
Angelo Garcia <angelogarcia@Mac-mini-de-Angelo.local>
2026-01-19 20:35:54 -0300

    fix: update supabase keys and create admin script

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types/database';
 
 const supabaseUrl = 'https://sfpzxxtptrlttvzymqto.supabase.co';
-const supabaseAnonKey = 'sbp_5bd5b28cc74c63aa9bf671e623fc05473b686a9b';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmcHp4eHRwdHJsdHR2enltcXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3NjUyMDEsImV4cCI6MjA4NDM0MTIwMX0.Nsydjy1Tt2uRcFY4vLi3x1iGLBwytzaLWwgUqoGoVZY';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
